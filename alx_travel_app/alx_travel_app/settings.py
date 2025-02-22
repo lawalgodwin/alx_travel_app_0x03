b816aae46ef6cf022889570a76fa123b24e95fc3
lawalgodwin <godwinl200@gmail.com>
2025-02-22 18:50:51 +0100

    Edit env.dist file

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-e3&jjg+*t+bu1tt)^9(q4i6u%uj!k4n^phrc!2()bm0ly%5+ju
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["booking-systems.onrender.com", "localhost", "127.0.0.1", "*"]
+ALLOWED_HOSTS = list(env("CHAPA_WEBHOOK_SECRET"))
 
 # Application definition
 
@@ -128,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
